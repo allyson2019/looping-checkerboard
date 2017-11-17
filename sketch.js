2f54ef73f6ae3dc72f2d9f9bc3753b428a993c8c
Allyson Dixon Duarte <allysond50340@students.laalliance.org>
2017-11-17 17:55:07 +0000

    checkers.

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(500,500);
+}
+function draw(){
+     background(0);
+     fill(250);
+     
+    var x = 40
+    var count = 0
+}
+
+while (x < count) {
+rect (x, x, 25, 25)
+}
+
+if (count % 2 === 0){
+fill (orange)
+else fill (black)
+    
+}
